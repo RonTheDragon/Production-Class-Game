046d74a544e8fad01d8f89dad0f80a312f849d1a
RonTheDragon <gweta.ron@gmail.com>
2022-07-18 09:22:48 +0300

    tiny fix

--- Assets/OurStuff/Scripts/Controllers/CharacterAI.cs
+++ Assets/OurStuff/Scripts/Controllers/CharacterAI.cs
@@ -203,28 +203,31 @@ public abstract class CharacterAI : MonoBehaviour , IpooledObject
 
     protected void AttackWall()
     {
-        Vector3 MoveTo;
+        if (!hp.Frozen)
+        {
+            Vector3 MoveTo;
 
-        float WallSmaller = GameManager.instance.WallLength * 0.8f;
-        if (GameManager.instance.WallFacingZ)
-        {
-            float Xpos = Mathf.Clamp(TheBody.position.x, TheWall.transform.position.x - (WallSmaller / 2), TheWall.transform.position.x + (WallSmaller / 2));
-            MoveTo = new Vector3(Xpos, TheBody.position.y, TheWall.transform.position.z);
-        }
-        else
-        {
-            float Zpos = Mathf.Clamp(TheBody.position.z, TheWall.transform.position.z - (WallSmaller / 2), TheWall.transform.position.z + (WallSmaller / 2));
-           // Debug.Log(Zpos);
-            MoveTo = new Vector3(TheWall.transform.position.x+ 2, TheBody.position.y, Zpos);
-        }
+            float WallSmaller = GameManager.instance.WallLength * 0.8f;
+            if (GameManager.instance.WallFacingZ)
+            {
+                float Xpos = Mathf.Clamp(TheBody.position.x, TheWall.transform.position.x - (WallSmaller / 2), TheWall.transform.position.x + (WallSmaller / 2));
+                MoveTo = new Vector3(Xpos, TheBody.position.y, TheWall.transform.position.z);
+            }
+            else
+            {
+                float Zpos = Mathf.Clamp(TheBody.position.z, TheWall.transform.position.z - (WallSmaller / 2), TheWall.transform.position.z + (WallSmaller / 2));
+                // Debug.Log(Zpos);
+                MoveTo = new Vector3(TheWall.transform.position.x + 2, TheBody.position.y, Zpos);
+            }
 
-        NMA.SetDestination(MoveTo);
-        float distance = Vector3.Distance(TheBody.position, MoveTo);
-        if (distance <= NMA.stoppingDistance + 1 || NMA.speed == 0)
-        {
-            RotateTowards(MoveTo);
-        }
-        eas.Attack(MoveTo,false);
+            NMA.SetDestination(MoveTo);
+            float distance = Vector3.Distance(TheBody.position, MoveTo);
+            if (distance <= NMA.stoppingDistance + 1 || NMA.speed == 0)
+            {
+                RotateTowards(MoveTo);
+            }
+            eas.Attack(MoveTo, false);
+        }else NMA.SetDestination(transform.position);
     }
 
     protected void Wander()
